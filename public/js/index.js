0e297b9359b93758d57539f8aacdb1893c552870
hugo costa <gohucosta23@hotmail.com>
2020-08-03 13:07:41 -0400

    Adding .gitignore file and disable button function

--- public/js/index.js
+++ public/js/index.js
@@ -4,25 +4,43 @@ import data from "./data.js";
 
 let index = 0;
 
+function buttonDisable() {
+
+    if(index === 0) {
+        $("#previousBtn").hide();
+    }
+    else if (index === data.length - 1){
+        $("#nextBtn").hide();
+    }
+}
+
 $("#nextBtn").on("click", function () {
+
     if (index < data.length - 1) {
         index += 1;
+        buttonDisable();
         $(".pageInfo").text(index + 1)
+        $("#previousBtn").show();
         Utils.populatePayeeInfo(index);
         Utils.populatePaymentInfo(index);
         Utils.populateRemittanceInfo(index)
-    }
+    } 
 });
+
 $("#previousBtn").on("click", function () {
+
     if (index > 0) {
         index -= 1;
+        buttonDisable();
         $(".pageInfo").text(index - 1)
+        $("#nextBtn").show();
         Utils.populatePayeeInfo(index);
         Utils.populatePaymentInfo(index);
         Utils.populateRemittanceInfo(index);
     }
 });
 
+buttonDisable();
 Utils.populatePayeeInfo(index);
 Utils.populatePaymentInfo(index);
 Utils.populateRemittanceInfo(index);
